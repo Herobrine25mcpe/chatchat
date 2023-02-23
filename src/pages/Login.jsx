8cb85219f4a9733db94821ae62ecf9ec19f50dda
Herobrine25mcpe <imherobrine25@gmail.com>
2023-02-23 18:32:44 +0530

    login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+const Login = () => {
+  return (
+    <div className="formContainer">
+        <div className="formWraper">
+
+            <span className="logo">Logo</span>
+            <span className="title">Login</span>
+
+            <form action="">              
+                <input type="email" placeholder='email'/>
+                <input type="password" placeholder='password'/>         
+            </form>
+            <button>Sign Up</button>
+            <p>Don't have have an account? Register</p>
+        </div>
+    </div>
+  )
+}
+
+export default Login
