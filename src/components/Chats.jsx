0d075bac9a31fc9229543e06c3979b1c8609c4e0
Herobrine25mcpe <imherobrine25@gmail.com>
2023-02-24 23:00:25 +0530

    chats

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -2,7 +2,47 @@ import React from 'react'
 
 const Chats = () => {
   return (
-    <div>Chats</div>
+    <div className='chats'>
+        <div className="userchat">
+        <img src="https://images.pexels.com/photos/3777622/pexels-photo-3777622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        <div className="userChatInfo">
+          <span>ptsss ptss</span>
+          <p>hello</p>
+        </div>
+      </div>
+
+      <div className="userchat">
+        <img src="https://images.pexels.com/photos/3777622/pexels-photo-3777622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        <div className="userChatInfo">
+          <span>ptsss ptss</span>
+          <p>hello</p>
+        </div>
+      </div>
+
+      <div className="userchat">
+        <img src="https://images.pexels.com/photos/3777622/pexels-photo-3777622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        <div className="userChatInfo">
+          <span>ptsss ptss</span>
+          <p>hello</p>
+        </div>
+      </div>
+
+      <div className="userchat">
+        <img src="https://images.pexels.com/photos/3777622/pexels-photo-3777622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        <div className="userChatInfo">
+          <span>ptsss ptss</span>
+          <p>hello</p>
+        </div>
+      </div>
+
+      <div className="userchat">
+        <img src="https://images.pexels.com/photos/3777622/pexels-photo-3777622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        <div className="userChatInfo">
+          <span>ptsss ptss</span>
+          <p>hello</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
