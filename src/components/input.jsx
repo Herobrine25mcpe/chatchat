2886ac1e8c058a0ae93199adfcdbfcd1e48fadd5
Herobrine25mcpe <imherobrine25@gmail.com>
2023-03-04 02:03:41 +0530

    responsive tablet

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Img from "../img/img.png"
+import Attach from "../img/attach.png"
 
 const Input = () => {
   return (
-    <div>input</div>
+    <div className='input'>
+      <input type="type" placeholder='Type here...' name="" id="" />
+      <div className="send">
+        <img src={Img} alt="" />
+        <input type="file" style={{display:"none"}} id="file" />
+        <label htmlFor="file">
+          <img src={Attach} alt="" />
+        </label>
+        <button>Send</button>
+      </div>
+    </div>
   )
 }
 
