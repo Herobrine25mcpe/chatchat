f96b29994bdc8445982da82e23af38eeab59b713
Herobrine25mcpe <imherobrine25@gmail.com>
2023-03-18 01:20:21 +0530

    firebase post 400 error fixed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,4 @@
-/*import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import "./style.scss"
 
-function App() {
-  return (
-    //<Home/>
-    <Register/>
-    //<Login/>
-  );
-}
-
-export default App;*/
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
