4d8bf4be5bc131ebbb3625f79c9b02700d6ee945
Herobrine25mcpe <imherobrine25@gmail.com>
2023-03-20 15:37:58 +0530

    navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,3 @@
-/*import React from 'react'
-
-const Navbar = () => {
-  return (
-    <div className='navbar'>
-        <span className="logo"> ChatChat </span>
-        <div className="user">
-            <img src="https://images.pexels.com/photos/1851164/pexels-photo-1851164.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
-            <span>Abhay</span>
-            <button>logout</button>
-        </div>
-    </div>
-  )
-}
-
-export default Navbar*/
 
 import React, { useContext } from 'react'
 import {signOut} from "firebase/auth"
