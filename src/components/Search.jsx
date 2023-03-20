5efaa32527786492b3a23c4c86956abbb89a3d02
Herobrine25mcpe <imherobrine25@gmail.com>
2023-03-20 15:38:33 +0530

    search  update

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,21 +1,4 @@
-/*import React from 'react'
 
-const Search = () => {
-  return (
-    <div className='search'>
-      <div className="searchform">
-        <input type="text"  placeholder='find user'/>
-      </div>
-
-      <div className="userchat">
-        <img src="https://images.pexels.com/photos/3777622/pexels-photo-3777622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-        <span>meow meow</span>
-      </div>
-    </div>
-  )
-}
-
-export default Search*/
 
 import React, { useContext, useState } from "react";
 import {
