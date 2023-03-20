fd945c91bcdc6d0e38d347de709d864aed4c17d8
Herobrine25mcpe <imherobrine25@gmail.com>
2023-03-20 15:39:13 +0530

    login  update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,24 +1,3 @@
-/*import React from 'react'
-const Login = () => {
-  return (
-    <div className="formContainer">
-        <div className="formWraper">
-
-            <span className="logo">ChatChat</span>
-            <span className="title">Login</span>
-
-            <form action="">              
-                <input type="email" placeholder='email'/>
-                <input type="password" placeholder='password'/>         
-            </form>
-            <button>Sign Up</button>
-            <p>Don't have have an account? Register</p>
-        </div>
-    </div>
-  )
-}
-
-export default Login*/
 
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
