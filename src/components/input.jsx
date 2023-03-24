b625a328a73893bd870b5030dc437f1f7decdb71
Herobrine25mcpe <imherobrine25@gmail.com>
2023-03-24 13:04:16 +0530

    conditional updates

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,26 +1,3 @@
-/*
-import React from 'react'
-import Img from "../img/img.png"
-import Attach from "../img/attach.png"
-
-const Input = () => {
-  return (
-    <div className='input'>
-      <input type="type" placeholder='Type here...' name="" id="" />
-      <div className="send">
-        <img src={Img} alt="" />
-        <input type="file" style={{display:"none"}} id="file" />
-        <label htmlFor="file">
-          <img src={Attach} alt="" />
-        </label>
-        <button>Send</button>
-      </div>
-    </div>
-  )
-}
-
-export default Input */
-
 
 import React, { useContext, useState } from "react";
 import Img from "../img/img.png";
